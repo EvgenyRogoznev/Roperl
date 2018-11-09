ea9e55eecfbe7d264922ca6e83c82aa96fafbc36
EvgenyRogoznev <89122015715@mail.ru>
2018-11-09 22:57:30 +0500

    444

--- 150.pl
+++ 150.pl
@@ -2,14 +2,17 @@
 use strict;
 use 5.010;
 sub greet{
+    my$arg=$_[0];
     our @named;
-    push (@named, $_);
+    push (@named, $arg);
     if (@named.length==1){
-        say "Hi $_,you are the first one here!"
+        say "Hi $arg,you are the first one here!"
     }
     else{ 
-        say "Hi $_ ! $[(@named.length)-1] is also here " ;
+        say "Hi $arg ! @named[0..(@named.length)-2] is also here " ;
         }
 };
+&greet("fred");
+&greet("gleb");
 &greet("fred");
 &greet("gleb");
