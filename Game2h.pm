d5a38e389179022dade86e0a3106858a3d4c398b
EvgenyRogoznev <89122015715@mail.ru>
2018-12-01 12:27:30 +0500

    nne

--- Game2h.pm
+++ Game2h.pm
@@ -1,11 +1,19 @@
 #!/usr/bin/env perl;
 package Game2;
+use base 'Exporter';
 use strict;
 use 5.016;
+our $WIN1=1;
+our $WIN2=2;
+our $WIN3=3;
+our $EXPORT_OK= qw($WIN1 $WIN2 $WIN3);
+our $INV_TURN=0;
+
+
 sub is_game_over {...};
 sub new {...};
 sub totxt {...};
-sub get_hn {...}
+sub get_hn {...};
 sub do_move {...};
 sub get_move {...};
 sub turn {...};
